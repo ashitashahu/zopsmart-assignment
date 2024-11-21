22bb1245207b38bbf1a08b3805358a77cc353105
unknown <shahuashita@gmail.com>
2024-11-21 20:59:00 +0530

    problem1 completed

--- problem1/index.js
+++ problem1/index.js
@@ -1,15 +1,9 @@
 function sum(num1) {
   return function (num2) {
-    return function (num3) {
-      return function (num4) {
-        return function (num5) {
-          return function () {
-            return num1 + num2 + num3 + num4 + num5;
-          };
-        };
-      };
-    };
+    if (num2 !== undefined) return sum(num1 + num2);
+    return num1;
   };
 }
 
+console.log(sum(3)(7)());
 console.log(sum(1)(2)(3)(4)(5)());
