f2ef506fcd3caaa3632b57b1157df6a43c3dbf57
unknown <shahuashita@gmail.com>
2024-11-21 21:13:54 +0530

    problem2 completed

---
+++ problem2/zopsmart-assignment/src/TrafficLight .jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const TrafficLight = () => {
+  const [color, setColor] = useState("green");
+
+  useEffect(() => {
+    let timer;
+
+    if (color === "green") {
+      timer = setTimeout(() => setColor("yellow"), 3000);
+    } else if (color === "yellow") {
+      timer = setTimeout(() => setColor("red"), 2000);
+    } else if (color === "red") {
+      timer = setTimeout(() => setColor("yellow"), 5000);
+    } else if (color === "yellow") {
+      timer = setTimeout(() => setColor("green"), 2000);
+    }
+
+    return () => clearTimeout(timer);
+  }, [color]);
+
+  return (
+    <div
+      style={{
+        width: "150px",
+        height: "150px",
+        backgroundColor: color,
+        textAlign: "center",
+      }}
+    ></div>
+  );
+};
+
+export default TrafficLight;
